d2e844e136ae8da99b18a617ab59057798b171f9
M.Lounes <lounes.mohamed@cdrin.com>
2021-05-31 17:03:01 -0400

    get functions list from the ikalas api

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+const fs = require("fs");
+require("dotenv").config({ path: __dirname + "/.env.public" });
+const envConfig = require("dotenv").parse(fs.readFileSync(".env"));
+for (const k in envConfig) {
+  process.env[k] = envConfig[k];
+}
 const readline = require("readline");
 const shellHistory = require("shell-history");
-var shelljs = require("shelljs");
+const shelljs = require("shelljs");
+const axios = require("axios");
+const chalk = require("chalk");
+const log = console.log;
+
+const API_URL = process.env.IKALAS_API_URL;
+
+if (process.env.ENABLE_IKALAS == "yes") {
+  axios.get(`${API_URL}/api/v1/functions`).then((result) => {
+    if (result != null && result.data != null) {
+      result.data.forEach(function (fn) {
+        suggestedCommands.push({
+          name: fn.nameFunction,
+          summary: fn.summaryFunction,
+        });
+      });
+    }
+  });
+}
 
 Array.prototype.unique = function () {
   return this.filter(function (value, index, self) {
@@ -15,9 +39,11 @@ var suggestedCommands = [];
 var historyCommands = shellHistory();
 historyCommands.forEach(function (historyCommand) {
   var found = suggestedCommands.find(function (element) {
-    return element == historyCommand;
+    return element.name == historyCommand;
   });
-  if (!found) suggestedCommands.push(historyCommand);
+  if (!found) {
+    suggestedCommands.push({ name: historyCommand });
+  }
 });
 var command = "";
 
@@ -47,14 +73,18 @@ process.stdin.on("keypress", (str, key) => {
 
     var previewSuggestedCommands = [];
     suggestedCommands.forEach(function (suggestedCommand) {
-      if (suggestedCommand.indexOf(command) >= 0) {
+      if (suggestedCommand.name.indexOf(command) >= 0) {
         previewSuggestedCommands.push(suggestedCommand);
       }
     });
 
     previewSuggestedCommands = previewSuggestedCommands.slice(0, 10);
     previewSuggestedCommands.forEach(function (suggestedCommand) {
-      console.log("\x1b[36m%s\x1b[0m", suggestedCommand);
+      log(
+        chalk.blue(suggestedCommand.name) +
+          " " +
+          chalk.italic(suggestedCommand.summary ? suggestedCommand.summary : "")
+      );
     });
   }
 });
