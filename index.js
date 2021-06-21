006d52d7dc64e2e45a3cead9045ae60333418d43
Hamed <hamed@ratiostack.com>
2021-06-21 16:57:58 +0100

    navagate through suggestions

--- index.js
+++ index.js
@@ -12,42 +12,23 @@ const chalk = require("chalk");
 const log = console.log;
 
 const API_URL = process.env.IKALAS_API_URL;
+const MAX_SUGGESTIONS = 10;
 
+let ikalas_commands = [];
 if (process.env.ENABLE_IKALAS == "yes") {
   axios.get(`${API_URL}/api/v1/functions`).then((result) => {
     if (result != null && result.data != null) {
-      result.data.forEach(function (fn) {
-        suggestedCommands.push({
+      ikalas_commands = result.data.map(function (fn) {
+        return {
           name: fn.nameFunction,
           summary: fn.summaryFunction,
-        });
+        };
       });
+
+      suggestedCommands.unshift(...ikalas_commands);
     }
   });
 }
-
-Array.prototype.unique = function () {
-  return this.filter(function (value, index, self) {
-    return self.indexOf(value) === index;
-  });
-};
-
-readline.emitKeypressEvents(process.stdin);
-console.log("\033[2J");
-var suggestedCommands = [];
-
-var historyCommands = shellHistory();
-historyCommands.forEach(function (historyCommand) {
-  var found = suggestedCommands.find(function (element) {
-    return element.name == historyCommand;
-  });
-  if (!found) {
-    suggestedCommands.push({ name: historyCommand });
-  }
-});
-var command = "";
-
-process.stdin.setRawMode(true);
 
 const showHelpMsg = () => {
   log("\nKLI 1.0\t\t\tKli Manual\t\t\tKLI(1)\n");
@@ -68,12 +49,78 @@ const showHelpMsg = () => {
 
 if (process.argv.length > 2 && ["--help", "-h"].includes(process.argv[2])) {
   showHelpMsg();
+  process.exit();
 }
 
+Array.prototype.unique = function () {
+  return this.filter(function (value, index, self) {
+    return self.indexOf(value) === index;
+  });
+};
+
+readline.emitKeypressEvents(process.stdin);
+console.log("\033[2J");
+
+const historyCommands = shellHistory();
+
+let suggestedCommands = historyCommands.map((element) => ({ name: element }));
+
+let previewSuggestedCommands = [];
+
+const previewSuggestions = (position = 0) => {
+  console.log("\033[2J");
+  console.log(">>" + command);
+  console.log("");
+  if (!command) {
+    suggestedCommands
+      .slice(0, MAX_SUGGESTIONS)
+      .forEach(function (suggestedCommand, index) {
+        if (position === index) {
+          return log(
+            chalk.red(suggestedCommand.name) +
+              " " +
+              chalk.italic(
+                suggestedCommand.summary ? suggestedCommand.summary : ""
+              )
+          );
+        }
+        log(
+          chalk.blueBright(suggestedCommand.name) +
+            " " +
+            chalk.italic(
+              suggestedCommand.summary ? suggestedCommand.summary : ""
+            )
+        );
+      });
+    return;
+  }
+  previewSuggestedCommands.forEach(function (suggestedCommand, index) {
+    if (position === index) {
+      return log(
+        chalk.red(suggestedCommand.name) +
+          " " +
+          chalk.italic(suggestedCommand.summary ? suggestedCommand.summary : "")
+      );
+    }
+    log(
+      chalk.blueBright(suggestedCommand.name) +
+        " " +
+        chalk.italic(suggestedCommand.summary ? suggestedCommand.summary : "")
+    );
+  });
+};
+
+var command = "";
+let position = 0;
+
+
+process.stdin.setRawMode(true);
 process.stdin.on("keypress", (str, key) => {
   if (key.ctrl && key.name === "c") {
-    process.exit();
-  } else if (key.name == "return") {
+    return process.exit();
+  }
+
+  if (key.name === "return") {
     console.log("\033[2J");
     shelljs.exec(command, function (status, output) {
       // console.log('Exit status:', status);
@@ -82,31 +129,39 @@ process.stdin.on("keypress", (str, key) => {
     // process.exit();
     console.log(">>");
     command = "";
-  } else {
-    if (key.name == "backspace") {
-      command = command.substring(0, command.length - 1);
-    } else {
-      command += key.sequence;
+    return;
+  }
+
+  if (key.name === "backspace") {
+    command = command.substring(0, command.length - 1);
+    previewSuggestions(0);
+    return;
+  }
+
+  if (!["up", "down", "backspace"].includes(key.name)) {
+    command += key.sequence;
+
+    previewSuggestedCommands = [];
+    previewSuggestedCommands = suggestedCommands
+      .filter((element) => element.name.indexOf(command) >= 0)
+      .slice(0, MAX_SUGGESTIONS);
+
+    previewSuggestions();
+  }
+
+  if (key.name == "down") {
+    if (position < MAX_SUGGESTIONS - 1) {
+      position++;
+      previewSuggestions(position);
     }
+    return;
+  }
 
-    console.log("\033[2J");
-    console.log(">>" + command);
-    console.log("");
-
-    var previewSuggestedCommands = [];
-    suggestedCommands.forEach(function (suggestedCommand) {
-      if (suggestedCommand.name.indexOf(command) >= 0) {
-        previewSuggestedCommands.push(suggestedCommand);
-      }
-    });
-
-    previewSuggestedCommands = previewSuggestedCommands.slice(0, 10);
-    previewSuggestedCommands.forEach(function (suggestedCommand) {
-      log(
-        chalk.blueBright(suggestedCommand.name) +
-          " " +
-          chalk.italic(suggestedCommand.summary ? suggestedCommand.summary : "")
-      );
-    });
+  if (key.name == "up") {
+    if (position > 0) {
+      position--;
+      previewSuggestions(position);
+    }
+    return;
   }
 });
